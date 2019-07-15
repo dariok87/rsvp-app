4981f4927f35664e0381f96206b50915c58bfba0
Dario Kemenovic <dkemenovic@gmail.com>
2019-07-15 13:59:34 +0200

    Pass new function to the Guest component

--- src/GuestList.js
+++ src/GuestList.js
@@ -11,7 +11,8 @@ const GuestLIst = props =>
 			isConfirmed={guest.isConfirmed} 
 			isEditing={guest.isEditing}
 			handleConfirmation={() => props.toggleConfirmationAt(index)}
-			handleToggleEditing={() => props.toggleEditingAt(index)} />
+			handleToggleEditing={() => props.toggleEditingAt(index)} 
+			setName={text => props.setNameAt(text, index)} />
 		)}
 	</ul>;
 
