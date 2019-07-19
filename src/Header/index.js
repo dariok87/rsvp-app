40e5241be9110a91807891d8671dd3a26f811e7f
Dario <dkemenovic@gmail.com>
2019-07-20 01:22:21 +0200

    Refactor code

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import GuestInputForm from '../GuestInputForm';
+import GuestInputForm from './GuestInputForm';
 
 const Header = props =>
 	<header>
