fc1f0d32449e93c667adae26fbed704e2eb262b0
Dario <dkemenovic@gmail.com>
2019-07-19 22:51:59 +0200

    Refactor code

--- src/App.js
+++ src/App.js
@@ -111,14 +111,6 @@ class App extends Component {
         <header>
           <h1>RSVP</h1>
           <p>A Treehouse App</p>
-          <form onSubmit={this.newGuestSubmitHandler}>
-              <input
-                type="text"
-                onChange={this.handleNameInput}
-                value={this.state.pendingGuest}
-                placeholder="Invite Someone" />
-              <button type="submit" name="submit" value="submit">Submit</button>
-          </form>
         </header>
         <div className="main">
           <div>
